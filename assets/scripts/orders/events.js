8f92db8680a28dde577c048a5b85796c9fde0e0a
vanduong28 <vanduong28@gmail.com>
2017-05-18 10:05:48 -0400

    Minor change to orders and installs stripe package
    Changes to orders to begin integrating Stripe. Installs stripe package

--- assets/scripts/orders/events.js
+++ assets/scripts/orders/events.js
@@ -2,24 +2,35 @@
 
 const orderApi = require('./api')
 const orderUi = require('./ui')
+const cart = require('../cart')
 
 const onCreateOrder = function (event) {
   event.preventDefault()
-
   console.log('(order/events.js) onCreateOrder ran!')
+  const data = {
+    order: {
+      items: cart.getItems()
+    }
+  }
+  console.log('(order/events.js) data is ', data)
+  // if (orderComplete.getId() === '') {
+  orderApi.createOrder(data)
+    .then(orderUi.onCreateOrderSuccess)
+    .catch(orderUi.onCreateOrderFailure)
+  // } else {
+  //   orderApi.updateOrder(orderComplete.getId(), data)
+  //     .then(orderUi.onUpdateOrderSuccess)
+  //     .catch(orderUi.onUpdateFailure)
+}
 
   // Need to get FILLED IN order object so that it can be passed to orderApi
   //  that will do a CREATE (POST) of that order to the database
 
-  console.log('(order/events.js) onCreateOrder - Order is: ', order)
+  // console.log('(order/events.js) onCreateOrder - Order is: ', order)
 
   // const orderId = $(this).data('id')
   // console.log('orderId is:', orderId)
-
-  orderApi.createOrder(order)
-    .then(orderUi.onCreateOrderSuccess)
-    .catch(orderUi.onCreateOrderFailure)
-}
+// }
 
 const addOrderHandlers = () => {
   // *** NEEDS TO BE MOVED ***
@@ -28,9 +39,10 @@ const addOrderHandlers = () => {
 
   // Listen for click event on the 'Create Order' button in the cart
   // use button id or class?
-  // $(document).on('click', '.checkoutBtn', onCreateOrder)
+  $(document).on('click', '#checkout-btn', onCreateOrder)
 }
 
 module.exports = {
-  addOrderHandlers
+  addOrderHandlers,
+  onCreateOrder
 }

a0730b36953c06ef8241053ab0f84e23e45acab0
jkjellan <jkjellan@gmail.com>
2017-05-18 13:05:31 -0400

    Builds order object to pass to ordersAPI ajax
    The product objects in the cart are stored in store.cart.
    I created a buildOrder function that takes that cart array of
    objects, and re-configures the object to the format needed by
    our orders schema for creating orders.

--- assets/scripts/products/ui.js
+++ assets/scripts/products/ui.js
@@ -1,5 +1,8 @@
 'use strict'
 const store = require('../store')
+// temporarily calling buildOrder from this file to
+// see console logs.
+const buildOrder = require('../orders/buildOrder.js')
 
 const getAllProductsSuccess = function (ajaxResponse) {
   console.log('(products/ui.js) getAllProductsSuccess ran!  Data is :', ajaxResponse)
@@ -19,19 +22,20 @@ const getAllProductsFailure = (error) => {
 }
 
 const getOneProductSuccess = function (ajaxResponse) {
-  console.log('(products/ui.js) getOneProductSuccess ran!  Data is :', ajaxResponse)
+  // console.log('(products/ui.js) getOneProductSuccess ran!  Data is :', ajaxResponse)
   // every time a user adds to the cart, a show action brings
   // back a product object, we we add to the store.cart array
   // start.cart is initialized as an empty array up on sign in success
   store.cart.products.unshift(ajaxResponse.product)
   const products = store.cart
-  console.log(products)
+  // console.log(products)
 
   $('#cartHandlebar').html('')
   const displayAllProducts = require('../templates/product-cart-listing.handlebars')
   $('#cartHandlebar').prepend(displayAllProducts(products))
   // const displayOneProduct = require('../templates/cart-listing.handlebars')
   // $('#cartHandlebar').prepend(displayOneProduct(product))
+  console.log(buildOrder.order())
 }
 
 const getOneProductFailure = (error) => {
